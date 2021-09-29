30fddac2b73fa366df740aa4eac0353e8a8bf5ee
Manoj Bhat <manudbhat@gmail.com>
2021-09-29 22:28:07 +0530

    covered some corner cases and done with styling

--- app.js
+++ app.js
@@ -6,27 +6,78 @@ var output = document.querySelector(".output")
 
 
 btnCheck.addEventListener("click",()=>{
-    var delta = Math.abs((initialStockPrice.value*noOfStocks.value) - (finalStockPrice.value*noOfStocks.value));
-    console.log(initialStockPrice.value,noOfStocks.value,finalStockPrice.value)
-    console.log(initialStockPrice.value)
-    console.log(finalStockPrice.value)
-    console.log()
-
-    var perc = ((delta/(initialStockPrice.value*noOfStocks.value))*100);
-
     var msg = "";
+    output.innerHTML = ""
+    output.style.display = "block";
+    
+    //checking the empty cases 
+    if(!initialStockPrice.value)
+    {
+        msg = "Please enter the Initial Stock Price";
+         output.innerHTML = msg;
+        return;
+    }
+    else if(parseInt(initialStockPrice.value) < 0)
+    {
+        
+        msg = "Initial Stock price cannot be negative";
+         output.innerHTML = msg;
+        return;
+    }
+    else if(!noOfStocks.value)
+    {
+        msg = "Please enter the Number of Stocks Purchased";
+         output.innerHTML = msg;
+        return;
+    }
+    else if(parseInt(noOfStocks.value) <= 0)
+    {
+         
+        msg = "Number of Stocks brought cannot be negative";
+         output.innerHTML = msg;
+        return;
+    }
+    else if(!finalStockPrice.value)
+    {
+        msg = "Please enter the Final Stock Price";
+         output.innerHTML = msg;
+        return;
+    }
+    else if(parseInt(finalStockPrice.value) < 0)
+    {
+        msg = "Final Stock price cannot be negative";
+         output.innerHTML = msg;
+        return;
+    }
+    output.innerHTML = msg;
+    var delta = Math.abs((initialStockPrice.value*noOfStocks.value) - (finalStockPrice.value*noOfStocks.value));
+    
+    if(parseInt(delta) == 0)
+    {
+        msg = "Your investment did not make any Profit/Loss.<br>"
+        msg += "Better Luck Next time!"
+        output.innerHTML = msg;
+        return;
+    }
+    
+   
+
+    var perc = Math.abs((delta/(initialStockPrice.value*noOfStocks.value))*100);
+
+    
+    
     if(parseInt(initialStockPrice.value)>parseInt(finalStockPrice.value))
     {
         // it is a loss
-        msg = "Sorry to break it to you.But you made a bad investment."
-        msg+= "You lost "+perc+"% of your initial investment which is "+delta+"$"
-        msg += " Stop investing in doge coin you punk"
+        msg = "Sorry to break it to you.But you made a bad investment.<br>"
+        msg+= "You lost "+perc+"% of your initial investment which is "+delta+"$<br>"
+       
     }
     else{
 
-        msg = "Yay.We are happy that you are able to make a good investment.";
-        msg += "Your stock made a growth of "+perc+"% which makes you exactly "+delta+"$ richer"
-        msg += "You go on ma boy"
+        msg = "Yay.We are happy that you are able to make a good investment.<br>";
+        msg += "Your stock made a growth of "+perc+"% which makes you exactly "+delta+"$ richer<br>"
+        
     }
     output.innerHTML = msg;
 })
